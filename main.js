afcf507de6a6e1ae217bdd53f9ba0ea49a9e9f00
sbsbj1 <lucasacevedo095@gmail.com>
2022-02-22 20:19:29 -0300

    BETA Version, need to apply some validations

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var rpsArray = [
     'PAPER',
     'SCISSOR'
 ]
-var count = 0;
+var Win = 0;
+var Lose = 0;
 
 
 function computerPlay () {
@@ -17,27 +18,36 @@ function playRound (playerSelection, computerSelection) {
     console.log(playerSelection)
     console.log(computerSelection)   // testing....
 
+    
     if(playerSelection !== computerSelection) {
         if(playerSelection === "ROCK" && computerSelection === "PAPER") {
-            console.log("You Lose! Paper beats Rock ")
+            Lose++;
+            return "You Lose! Paper beats Rock";
+
         }
         else if(playerSelection === "ROCK" && computerSelection === "SCISSOR") {
-            console.log("You Win! Rock beats Scissor")
-            return count++;
+            Win++;
+            return "You Win! Rock beats Scissor";
+            
         } 
         else if(playerSelection === "PAPER" && computerSelection === "ROCK") {
-            console.log("You Win! Paper beats Rock")
-            return count++;
+            Win++;
+            return "You Win! Paper beats Rock";
+            
         } 
         else if(playerSelection === "PAPER" && computerSelection === "SCISSOR") {
-            console.log("You Lose! Scissor beats Paper")
+            Lose++;
+            return "You Lose! Scissor beats Paper";
+            
         } 
         else if(playerSelection === "SCISSOR" && computerSelection === "ROCK") {
-            console.log("You Lose! Rock beats Scissor")
+            Lose++;
+            return "You Lose! Rock beats Scissor";
         } 
         else if(playerSelection === "SCISSOR" && computerSelection === "PAPER") {
-            console.log("You Win! Scissor beats Paper")
-            return count++;
+            Win++;
+            return "You Win! Scissor beats Paper";
+            
         } 
     } 
     else if(playerSelection === computerSelection) {
@@ -49,16 +59,31 @@ function playRound (playerSelection, computerSelection) {
 
 }
 
-const playerSelection = prompt("Choose your shit").toUpperCase();
-const computerSelection = computerPlay();
- console.log(playRound(playerSelection, computerSelection));  // testing playRound function
+//const playerSelection = prompt("Choose your shit").toUpperCase();
+//const computerSelection = computerPlay();
+// console.log(playRound(playerSelection, computerSelection));  // testing playRound function
 
-/* function game() {
-    for (let i = 0; i < 5; i++) {
+
+function game() {
+
+
+     for (let i = 0; i < 5; i++) {
         
-        playRound(playerSelection, computerSelection);
-        
-    }
+        const playerSelection = prompt("Choose your shit").toUpperCase();
+        const computerSelection = computerPlay(); 
+        console.log(playRound(playerSelection, computerSelection));
+
+     }
+    if(Win > Lose) {
+        console.log("CONGRATS! YOU ARE THE WINNER!!"); 
+        }
+        else if(Lose > Win){
+        console.log("YOU LOSE");
+        }
+        else {
+        console.log("ITS A DRAW!!");
+        } 
+
 }
 
-game(); */
+game();  
